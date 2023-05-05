5064478eaf38e5df82279e9e1cbc8ec9ac01f268
Divyanshu Singh <cgcdivyanshu@gmail.com>
2023-05-05 21:22:05 +0530

    second commit

---
+++ New folder/que2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class que2 {
+    public static void main(String[] args){
+            Scanner s=new Scanner(System.in);
+            System.out.println("side of square:");
+            int a=s.nextInt();
+            int area=a*a;
+            System.out.println("area ="+area);
+        }
+      }
