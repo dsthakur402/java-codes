80e5aa11d7dedfa9deb5dbe3f5aeae30a68e4309
Divyanshu Singh <cgcdivyanshu@gmail.com>
2023-06-08 17:55:22 +0530

    recursion added

---
+++ recursion/factorial.java
@@ -0,0 +1,16 @@
+public class factorial
+{
+    public static int fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n*fact(n-1);
+        
+    }
+    public static void main(String[] args) 
+    {
+     System.out.println(fact(5));   
+    }
+}
---
+++ recursion/naturalSum.java
@@ -0,0 +1,15 @@
+public class naturalSum 
+{
+    public static int sum(int n)
+    {
+        if (n==1)
+        {
+            return 1;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println(sum(5));
+    }
+}
